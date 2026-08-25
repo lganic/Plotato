01cf26c91e960fc9c1bf2f5d1162cb18a85a4a26
lganic <55366941+lganic@users.noreply.github.com>
2026-08-25 09:38:12 -0400

    Added anchor implementation.

---
+++ include/Plotato/util/Anchor.hpp
@@ -0,0 +1,20 @@
+#ifndef PLOTATO_ANCHOR_HPP
+#define PLOTATO_ANCHOR_HPP
+
+namespace plotato {
+
+enum Anchor {
+    TOP_LEFT,
+    TOP_CENTER,
+    TOP_RIGHT,
+    MIDDLE_LEFT,
+    MIDDLE_CENTER,
+    MIDDLE_RIGHT,
+    BOTTOM_LEFT,
+    BOTTOM_CENTER,
+    BOTTOM_RIGHT
+};
+
+}
+
+#endif // PLOTATO_ANCHOR_HPP

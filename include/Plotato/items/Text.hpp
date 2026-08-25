d3f8360e56d79dc5205e909075e45619cddb0467
lganic <55366941+lganic@users.noreply.github.com>
2026-08-25 10:08:10 -0400

    Added initial files for text.

---
+++ include/Plotato/items/Text.hpp
@@ -0,0 +1,27 @@
+#ifndef PLOTATO_TEXT_HPP
+#define PLOTATO_TEXT_HPP
+
+#include <Plotato/items/PlotItem.hpp>
+#include <Plotato/util/Anchor.hpp>
+#include <Plotato/util/GraphBounds.hpp>
+#include <Plotato/util/StyleStructs.hpp>
+#include <string>
+
+namespace plotato {
+
+class Text : public PlotItem
+{
+
+    bool absolute = false;
+    std::string text;
+    Anchor anchor;
+
+    public:
+        Text(double x, double y, std::string text, TextStyle style);
+        void draw(RenderContext& ctx);
+        GraphBounds bounds();
+};
+
+}
+
+#endif // PLOTATO_TEXT_HPP

37d2c2a385626fd5fd7e4195aee07fac8c19df54
lganic <55366941+lganic@users.noreply.github.com>
2026-09-08 15:48:30 -0400

    Added implementation of marker drawing to line plot

--- src/items/LinePlot.cpp
+++ src/items/LinePlot.cpp
@@ -13,6 +13,15 @@ LinePlot::LinePlot(std::vector<double> x_data, std::vector<double> y_data, PlotS
 void LinePlot::draw(RenderContext& ctx) {
     // Pass our line data to the graph renderer. 
     ctx.renderer.draw_polyline(x, y, style);
+
+    if (marker_active) {
+        
+        // Loop over all the x data, and plot each individual element using the graphrenderer.  
+        for (size_t i = 0; i < x.size(); i ++) {
+            ctx.renderer.draw_marker(x[i], y[i], style.marker);
+        }
+
+    }
 }
 
 GraphBounds LinePlot::bounds(){
